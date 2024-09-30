9be33e143f050bbf841f8625c472ee2012d8ed20
dinosshan <dinosshan@birdsview.no>
2024-09-30 09:58:37 +0200

    speed up image undistortion by a factor of 10 ish

--- scripts/live_processing.py
+++ scripts/live_processing.py
@@ -14,13 +14,16 @@ class LiveProcessing():
         
         self.topic_name = '/insta_image_yuv'
         self.undistort = rospy.get_param('undistort', False)
-        self.K = np.asarray(rospy.get_param("K", [[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
-        self.D = np.asarray(rospy.get_param("D", [0, 0, 0, 0]))
+        self.K = np.asarray(rospy.get_param("K", [[1, 0, 0], [0, 1, 0], [0, 0, 1]])).astype(np.float32)
+        self.D = np.asarray(rospy.get_param("D", [0, 0, 0, 0])).astype(np.float32)
 
         self.image_sub = rospy.Subscriber(self.topic_name, Image, self.processing)
         self.front_image_pub = rospy.Publisher('front_camera_image/compressed', CompressedImage, queue_size=10)
         self.back_image_pub = rospy.Publisher('back_camera_image/compressed', CompressedImage, queue_size=10)
 
+        h, w = 1152, 2304  # Hardcoded image size for Insta360 X3
+        self.map1_front, self.map2_front = cv2.fisheye.initUndistortRectifyMap(self.K, self.D, np.eye(3), self.K, (w//2, h), cv2.CV_32FC1)
+        self.map1_back, self.map2_back = cv2.fisheye.initUndistortRectifyMap(self.K, self.D, np.eye(3), self.K, (w//2, h), cv2.CV_32FC1)
 
     def processing(self, msg):
         try:
@@ -40,8 +43,8 @@ class LiveProcessing():
         
             #Live Undistortion
             if self.undistort:
-                front_image = undistort_image(front_image, self.K, self.D)
-                back_image = undistort_image(back_image, self.K, self.D)
+                front_image = cv2.remap(front_image, self.map1_front, self.map2_front, interpolation=cv2.INTER_LINEAR)
+                back_image = cv2.remap(back_image, self.map1_back, self.map2_back, interpolation=cv2.INTER_LINEAR)
 
             #Convert to compressed image message
             front_compressed_msg = compress_image_to_msg(front_image, msg.header.stamp)
